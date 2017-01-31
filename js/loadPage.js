a54cd38656d6d4eef71106abaaac8ea5c79ed56a
AprioriC <andreas97fm@yandex.ru>
2017-01-31 19:10:12 +0300

    Add really perfect timer and other stuff

--- js/loadPage.js
+++ js/loadPage.js
@@ -5,14 +5,14 @@
 
 var myDate = Date.now();
 
+//var amountOfTicks = 0;
 //значения indexpage 0-index.html , 1- map.html, 2-timer.html,
 //необходимы для завершения таймера при переходе назад
 var indexpage = -1;
 
-function getSeconds() {
+function getMiliSeconds() {
     var newDate = Date.now();
     var mySeconds = newDate - myDate;
-    mySeconds = Math.round(mySeconds / 1000)
     return mySeconds;
 };
 
@@ -25,6 +25,8 @@ function upTimer() {
     var myTmrMin = $(".TimerMinVal");
     var myTmrSec = $(".TimerSecVal");
 
+    //amountOfTicks=0;
+
     var myTmrMinName = $(".timerMinName");
     var myTmrSecName = $(".timerSecName");
 
@@ -35,22 +37,28 @@ function upTimer() {
 
     timerId = setInterval(function () {
         changeTimerValues(myTmrMin, myTmrSec, myTmrMinName, myTmrSecName, minutesContainer, secondsContainer);
-    }, 1000)
+
+    }, 100)
 };
 
 function changeTimerValues(myTmrMin, myTmrSec, myTmrMinName, myTmrSecName, minutesContainer, secondsContainer) {
     //изменение тайсера
-    var valSec = getSeconds();
+
+    var valMiliSec =getMiliSeconds();
+    var valSec = Math.round(valMiliSec/1000);
     var minut = valSec / 60;
-    var second = valSec - 60 * Math.floor(minut);
-    myTmrMin.html(checkNum(Math.floor(minut)) + "");
-    myTmrSec.html(checkNum(second) + "");
-    var secGradus = (second / 60) * 360;
+    //иногда вылезал баг, из-за которого не изменялся html в счеткчике
+    //после удаления проверки (10 тик? перерисовка: не надо перерисовывать) всё стало нормально
+        var second = valSec - 60 * Math.floor(minut);
+        myTmrMin.html(checkNum(Math.floor(minut)) + "");
+        myTmrSec.html(checkNum(second) + "");
+
+    var secGradus = (valMiliSec/1000 / 60) * 360;
     var minGradus = ((minut % 60) / 60) * 360;
     secondsContainer.css("transform", "rotateZ(" + secGradus + "deg)");
     minutesContainer.css("transform", "rotateZ(" + minGradus + "deg)");
 
-
+    //amountOfTicks++;
     //myTmrMinName.html(declOfNum(minut, minTitles ));
     // myTmrSecName.html(declOfNum(second, secTitles ));
 };
